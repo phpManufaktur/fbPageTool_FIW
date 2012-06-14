3e16c74da4ac5af80c06d38b5ca5434331eaea57
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-14 07:14:48 +0200

    Removed WebsiteBaker version check

--- precheck.php
+++ precheck.php
@@ -34,12 +34,6 @@ else {
 
 // Checking Requirements
 
-$PRECHECK['WB_VERSION'] = array(
-        'VERSION' => '2.8', 'OPERATOR' => '>='
-        );
-$PRECHECK['LEPTON_VERSION'] = array(
-        'VERSION' => '1.1', 'OPERATOR' => '>='
-        );
 $PRECHECK['PHP_VERSION'] = array(
         'VERSION' => '5.2.0', 'OPERATOR' => '>='
         );
